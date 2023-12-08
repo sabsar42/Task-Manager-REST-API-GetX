24a3ce982d00e9b96e230bbdba42018d5145f30f
Shakib Absar <sabsar42@gmail.com>
2023-12-08 17:45:31 +0600

    State Management of getX will be Added

--- lib/ui/screens/sign_up_screen.dart
+++ lib/ui/screens/sign_up_screen.dart
@@ -238,11 +238,4 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 }
 
-// FOR RegEx
 
-// bool validateEmail(String value) {
-//   Pattern pattern =
-//       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-//   RegExp regex = new RegExp(pattern);
-//   return (!regex.hasMatch(value)) ? false : true;
-// }
